897000fc785b5d15438cbb88c92f0d46d6803da7
Bernadete <bernadete98@hotmail.com>
2019-11-19 14:41:04 -0300

    apppp

--- app/src/main/java/com/ftec/poa/myapp10/MainActivity.java
+++ app/src/main/java/com/ftec/poa/myapp10/MainActivity.java
@@ -1,18 +1,26 @@
 package com.ftec.poa.myapp10;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.ShareActionProvider;
 import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends ListActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     private ListaDAO listaDAO;
     private EditText edtDescricao;
 
@@ -20,6 +28,74 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Toolbar Title");
+        getSupportActionBar().setSubtitle("Toolbar Subtitle");
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Infla o menu com os botões da action bar
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+
+            // SearchView
+            MenuItem searchItem = menu.findItem(R.id.action_search);
+            SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+            searchView.setOnQueryTextListener(onSearch());
+
+            // ShareActionProvider
+            MenuItem shareItem = menu.findItem(R.id.action_share);
+            ShareActionProvider share = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+            share.setShareIntent(getDefaultIntent());
+
+            return true;
+        }
+        // Intent que define o conteúdo que será compartilhado
+        private Intent getDefaultIntent() {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/*");
+            intent.putExtra(Intent.EXTRA_TEXT, "Texto para compartilhar");
+            return intent;
+        }
+
+        private SearchView.OnQueryTextListener onSearch() {
+            return new SearchView.OnQueryTextListener(){
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    toast("Buscar o texto: " + query);
+                    return false;
+                }
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
+            };
+        }
+        /*
+    Método para indicar o que deve ser feito ao se clicar
+    em algum item do menu
+     */
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == R.id.action_search) {
+                toast("Clicou em Search!");
+                return true;
+            } else if (id == R.id.action_refresh) {
+                toast("Clicou em Refresh!");
+                return true;
+            }else if (id == R.id.action_settings) {
+                toast("Clicou em Settings!");
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+        private void toast(String msg) {
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        }
 
         listaDAO = new ListaDAO(this);
         listaDAO.abrir();
@@ -32,12 +108,14 @@ public class MainActivity extends ListActivity {
 
         this.getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
-        edtDescricao = (EditText)findViewById(R.id.editText1);
+        edtDescricao = (EditText)findViewById(R.id.editText);
 
         Toast.makeText(this, "Toque rapidamente em um item " +
                         "para marcá-lo. Toque novamente para excluí-lo.",
                 Toast.LENGTH_LONG).show();
     }
+
+
 
     @Override
     protected void onPause() {
